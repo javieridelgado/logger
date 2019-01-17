1603cb091b84539f17cd92c7ae59a2c031eb769b
Javier Delgado <javier.i.delgado@gmail.com>
2019-01-17 15:09:51 +0100

    done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var https = require("https");
 var app = express();
+
+var options = {
+  hostname: 'wd3-impl-services1.workday.com',
+  port: 443,
+  path: '/ccx/service/verisure1/Human_Resources/v31.2'
+}
 
 app.use(function(req, res, next) {
   var data = '';
@@ -15,18 +22,84 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  var data = '';
+
+  options.method = 'GET';
+
+  console.log(options.method);
   console.log(req.body);
+
+  var wdayCall = https.request(options, (r) => {
+    console.log(`statusCode: ${r.statusCode}`)
+
+    r.on('data', (d) => {
+      data += d;
+    });
+
+    r.on('end', function() {
+      console.log('response: ' + data);
+      res.send(data);
+    });
+  });
 });
 
 app.post('/', function(req, res) {
-  res.send('Logged');
+  var data = '';
+
+  options.method = 'POST';
+
+  console.log(options.method);
   console.log(req.body);
+
+  var wdayCall = https.request(options, (r) => {
+    console.log(`statusCode: ${r.statusCode}`)
+
+    r.on('data', (d) => {
+      data += d;
+    });
+
+    r.on('end', function() {
+      console.log('response: ' + data);
+      res.send(data);
+    });
+  });
+
+  wdayCall.on('error', (error) => {
+    console.error(error)
+  })
+
+  wdayCall.write(req.body);
+  wdayCall.end();
 });
 
 app.put('/', function(req, res) {
-  res.send('Logged');
+  var data = '';
+
+  options.method = 'PUT';
+
+  console.log(options.method);
   console.log(req.body);
+
+  var wdayCall = https.request(options, (r) => {
+    console.log(`statusCode: ${r.statusCode}`)
+
+    r.on('data', (d) => {
+      console.log('receiving data: ' + d);
+      data += d;
+    });
+
+    r.on('end', function() {
+      console.log('response: ' + data);
+      res.send(data);
+    });
+  });
+
+  wdayCall.on('error', (error) => {
+    console.error(error)
+  })
+
+  wdayCall.write(req.body);
+  wdayCall.end();
 });
 
 app.listen(3001, function() {
